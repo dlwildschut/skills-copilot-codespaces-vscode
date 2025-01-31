ffdb9220779867f011eaa8a214a3cbde55342481
dlwildschut <daniel-laurens.wildschut@atos.net>
2025-01-31 15:22:30 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let member = {
+      name: 'John',
+      age: 30,
+      skills: ['JavaScript', 'React', 'Node'],
+      calculate: calculateNumbers
+    };
+    return member;
+  }
