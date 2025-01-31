6efefc2cc58edaf8baa55b5d81cddeff7648b569
dlwildschut <daniel-laurens.wildschut@atos.net>
2025-01-31 15:27:25 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,38 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves a static file called comments.html. The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+// Create a web server
+// Create a web server that listens on port 3000 and serves a static file called comments.html. The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+// The file is located in the /public folder. Use the fs module to read the file and serve it as the response.
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    fs.readFile('./public/comments.html', 'utf8
