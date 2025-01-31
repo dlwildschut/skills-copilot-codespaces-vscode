6ffe1a51de360db547ad0ac6fc93de091025ee1c
dlwildschut <daniel-laurens.wildschut@atos.net>
2025-01-31 15:21:18 +0000

    Copilot second commit

--- skills.js
+++ skills.js
@@ -1,3 +1,13 @@
 function calculateNumbers(var1, var2) {
   return var1 + var2;
 }
+
+function skillsMember() {
+  let member = {
+    name: 'John',
+    age: 30,
+    skills: ['JavaScript', 'React', 'Node'],
+    calculate: calculateNumbers
+  };
+  return member;
+}
